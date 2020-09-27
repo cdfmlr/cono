12f8832d4f4c66bf116ef2877e62bed810ef75bb
cdfmlr <cdfmlr@163.com>
2020-09-27 22:36:01 +0800

    feat(student): 实现 config 基本框架，实现 Database 配置。

---
+++ student/config/config.go
@@ -0,0 +1,18 @@
+package config
+
+// confDatabase is a struct for Database configures. Provides DSN (Data Source Name):
+//    [username[:password]@][protocol[(address)]]/dbname
+// Refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details.
+type ConfDatabase struct {
+	Username string
+	Password string
+	Protocol string
+	Address  string
+	DBName   string
+}
+
+// Conf is a struct wraps all configures.
+// field XXX -> <type ConfXXX struct>
+type Conf struct {
+	Database ConfDatabase
+}
