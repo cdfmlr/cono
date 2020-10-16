0e8103bac786d3d5e3bd58ded7b73d2022e44467
cdfmlr <cdfmlr@163.com>
2020-10-16 22:13:56 +0800

    feat(course): transport/wx: start http serve in initWx()

--- course/transport/wx.go
+++ course/transport/wx.go
@@ -4,6 +4,7 @@ import (
 	"conocourse/config"
 	"github.com/cdfmlr/wxofficialaccount"
 	log "github.com/sirupsen/logrus"
+	"net/http"
 )
 
 var WxOfficialAccount *wxofficialaccount.WxOfficialAccount
@@ -15,4 +16,12 @@ func initWx() {
 		config.WxOfficialAccount.AppID,
 		config.WxOfficialAccount.AppSecret,
 		config.WxOfficialAccount.Token)
+
+	log.WithField("address", config.WxOfficialAccount.Address).Info("boot WxOfficialAccount Server.")
+	err := http.ListenAndServe(config.WxOfficialAccount.Address, WxOfficialAccount)
+	if err != nil {
+		log.WithField("address", config.WxOfficialAccount.Address).
+			WithError(err).
+			Fatal("Failed to start WxOfficialAccount Server.")
+	}
 }
