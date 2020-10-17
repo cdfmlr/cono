27c8a01a5ee1283d6caa4acbcec7af2f684663bb
cdfmlr <cdfmlr@163.com>
2020-10-17 13:21:57 +0800

    fix(course): transport/wx: http serve in initWx() => WxHttpServe()
    bug from test: server startup from `initWx()` causes an always-not-implemented handler: SetMessageHandler after ListenAndServe will not be active.
    
    This commit solved the bug above by:
    
    - remove server startup from `initWx()`
    - server startup by new API: `func WxHttpServe(handler)`

--- course/transport/wx.go
+++ course/transport/wx.go
@@ -3,6 +3,7 @@ package transport
 import (
 	"conocourse/config"
 	"github.com/cdfmlr/wxofficialaccount"
+	"github.com/silenceper/wechat/v2/officialaccount/message"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 )
@@ -16,8 +17,17 @@ func initWx() {
 		config.WxOfficialAccount.AppID,
 		config.WxOfficialAccount.AppSecret,
 		config.WxOfficialAccount.Token)
+}
 
-	log.WithField("address", config.WxOfficialAccount.Address).Info("boot WxOfficialAccount Server.")
+// WxHttpServe 开启微信公众号的 http 消息接收、回复服务。
+func WxHttpServe(handler func(msg message.MixMessage) *message.Reply) {
+	WxOfficialAccount.SetMessageHandler(handler)
+
+	log.WithFields(log.Fields{
+		"address": config.WxOfficialAccount.Address,
+		"handler": handler,
+	}).Info("boot WxOfficialAccount Server.")
+
 	err := http.ListenAndServe(config.WxOfficialAccount.Address, WxOfficialAccount)
 	if err != nil {
 		log.WithField("address", config.WxOfficialAccount.Address).
